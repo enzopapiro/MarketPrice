f6458f85981e8082a8425ab014e6ba82ef1295e3
Enzo Papiro <enzopapiro@googlemail.com>
2023-02-16 16:16:54 +0000

    AtomicReadWriteSynchroniser bug fix.

--- src/main/java/org/enzopapiro/marketprice/util/concurrency/AtomicReadWriteSynchroniser.java
+++ src/main/java/org/enzopapiro/marketprice/util/concurrency/AtomicReadWriteSynchroniser.java
@@ -1,20 +1,13 @@
 package org.enzopapiro.marketprice.util.concurrency;
 
 import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicLong;
 
-/**
- * This class is initialised with an int, write win probability parameter.
- * The class makes of AtomicInteger representing the current thread that hold a
- * read or write lock(lockless/non-blocking).
- *
- * The method exposes tryWrite, tryRead and done to allow a few threads to co-ordinate themselves
- * around a shared resource without blocking and offers a way to avoid thread starvation in a process that
- * requires continually spinning threads to mitigate latency.
- *
- */
 public class AtomicReadWriteSynchroniser {
     private final AtomicLong winnerThreadId = new AtomicLong();
+    private final AtomicInteger numReaders = new AtomicInteger();
+
     private final int writeWinProbability;
 
     public AtomicReadWriteSynchroniser(int writeWinProbability) {
@@ -23,24 +16,39 @@ public class AtomicReadWriteSynchroniser {
 
     public boolean tryWrite() {
         long currentThreadId = Thread.currentThread().getId();
-        return winnerThreadId.compareAndSet(0L, currentThreadId);
+        if (numReaders.get() > 0) {
+            return false;
+        }
+        if (winnerThreadId.get() == currentThreadId) {
+            return true;
+        }
+        if (ThreadLocalRandom.current().nextInt(100) < writeWinProbability) {
+            boolean success = winnerThreadId.compareAndSet(0L, currentThreadId);
+            if (success) {
+                numReaders.set(0);
+                return true;
+            }
+        }
+        return false;
     }
 
     public boolean tryRead() {
         long currentThreadId = Thread.currentThread().getId();
         long existingWinnerThreadId = winnerThreadId.get();
         if (existingWinnerThreadId == 0L || existingWinnerThreadId == currentThreadId) {
+            numReaders.incrementAndGet();
             return true;
-        }
-        int randomNumber = ThreadLocalRandom.current().nextInt(100) + 1;
-        if (randomNumber > writeWinProbability) {
-            // so far reader has won but do a last check to make sure a writer hasn't sneaked in.
-            return existingWinnerThreadId == winnerThreadId.get();
         }
         return false;
     }
 
     public void done() {
-        winnerThreadId.set(0L);
+        if (numReaders.get() > 0) {
+            numReaders.decrementAndGet();
+        } else {
+            winnerThreadId.set(0L);
+        }
     }
 }
+
+
